68453a5de8b8f13d27f54c3138d962c249f70cf1
Chaitanya Tripathi <93254346+chaitanyatripathi0618@users.noreply.github.com>
2023-06-13 10:38:40 +0530

    Create EncapsulationApp.java

---
+++ EncapsulationApp.java
@@ -0,0 +1,10 @@
+public class EncapsulationApp {
+    public static void main(String[] args){
+        Encapsulation student1 = new Encapsulation();//constructuctor
+        student1.setId(1);
+        student1.setName("John ");
+        student1.setEmail("john@gmailcom ");
+        System.out.println(student1);
+    }
+    
+}
