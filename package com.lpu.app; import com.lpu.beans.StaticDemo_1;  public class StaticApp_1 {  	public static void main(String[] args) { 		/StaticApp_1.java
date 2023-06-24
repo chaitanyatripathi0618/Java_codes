9a18a0ce2207ad40a7ece3c580504d5ca881a0bf
Chaitanya Tripathi <93254346+chaitanyatripathi0618@users.noreply.github.com>
2023-06-24 23:38:59 +0530

    Create StaticApp_1.java

---
+++ package com.lpu.app; import com.lpu.beans.StaticDemo_1;  public class StaticApp_1 {  	public static void main(String[] args) { 		/StaticApp_1.java
@@ -0,0 +1,17 @@
+package com.lpu.app;
+import com.lpu.beans.StaticDemo_1;
+
+public class StaticApp_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticDemo_1 d1= new StaticDemo_1("chirag");
+		d1.details();
+		
+		
+		
+		
+
+	}
+
+}
